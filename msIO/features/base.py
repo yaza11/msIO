102e7f7a47ce98466fe463f56dda528e468b6d0e
yaza11 <116643078+yaza11@users.noreply.github.com>
2025-08-11 10:43:24 +0200

    adapt to sqlalchemy

--- msIO/features/base.py
+++ msIO/features/base.py
@@ -1,15 +1,57 @@
-class FeatureBaseClass(SqlBaseClass):
-    rt_seconds: float = None
-    rt_minutes: float = None
+from typing import Optional
+
+from sqlalchemy import Float
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+def get_py_dtypes_for_obj(obj: object) -> dict[str, type]:
+    dtypes: dict[str, type] = {}
+    for attr_name, mappable in obj.__annotations__.items():
+        # pull out the dtype from Mapped
+        t = mappable.__args__[0]
+        # pull out arg from optional
+        if hasattr(t, '_name') and (t._name == 'Optional'):
+            t = t.__args__[0]
+
+        dtypes[attr_name] = t
+    return dtypes
+
+
+class SqlBaseClass(DeclarativeBase):
+    pass
+
+
+class FeatureBaseClass:
+    """Some universal functionality for Feature objects."""
+    __abstract__ = True  # no table created for this class
+
+    rt_seconds: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
+    rt_minutes: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
+
+    @classmethod
+    def py_types(cls) -> dict[str, type]:
+        return get_py_dtypes_for_obj(cls)
 
     @classmethod
     def _convert_type(cls, attr: str, val):
         """Use annotations to get desired type"""
-        return cls.__annotations__[attr](val)
+        return cls.py_types()[attr](val)
 
-    def __post_init__(self):
-        if not self.rt_seconds and self.rt_minutes:
-            self.rt_seconds = self.rt_minutes * 60
+    def __init__(self, **kwargs):
+        super().__init__()
 
-        if not self.rt_minutes and self.rt_seconds:
-            self.rt_minutes = self.rt_seconds / 60
+        # use type annotations to convert input kwargs to right types
+        kwargs_converted = {k: self._convert_type(k, v) for k, v in kwargs.items()}
+        self.__dict__ |= kwargs_converted
+        if kwargs_converted.get('rt_minutes') is not None and kwargs_converted.get('rt_seconds') is None:
+            self.rt_seconds = kwargs_converted['rt_minutes'] * 60
+        elif kwargs_converted.get('rt_seconds') is not None and kwargs_converted.get('rt_minutes') is None:
+            self.rt_minutes = kwargs_converted['rt_seconds'] / 60
+
+
+if __name__ == '__main__':
+    # test type conversion and other population
+    fbc1 = FeatureBaseClass(rt_seconds=60)
+    fbc2 = FeatureBaseClass(rt_seconds="60")
+
+    assert fbc1.rt_minutes == fbc2.rt_minutes == 1
