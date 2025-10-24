1cbd717b632880a2aad352ddcce00f7cc75f6731
yaza11 <116643078+yaza11@users.noreply.github.com>
2025-10-24 16:54:10 +0200

    version updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='msIO',
-    version='0.0.18',
+    version='0.0.19',
     url='',
     author='Yannick Zander',
     author_email='yannick.zander@gmail.com',
