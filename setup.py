1d774f1bae35ee3ae5eeee1ce1bb6e42438c8cd9
yaza <yannick.zander@googlemail.com>
2025-12-09 13:59:18 +0100

    update LipidCalculator dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='msIO',
-    version='0.0.29',
+    version='0.0.31',
     url='',
     author='Yannick Zander',
     author_email='yannick.zander@gmail.com',
