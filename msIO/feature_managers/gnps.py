ae696a26d667d71f7a876e2da8ae31a9f2c9ba95
yaza11 <116643078+yaza11@users.noreply.github.com>
2026-05-20 10:34:25 +0200

    fix gnps feature initialization

--- msIO/feature_managers/gnps.py
+++ msIO/feature_managers/gnps.py
@@ -1,13 +1,12 @@
 import os
 from functools import cached_property
-from typing import Iterable
 
 import networkx as nx
 import numpy as np
-from tqdm import tqdm
+import pandas as pd
 
 from msIO.feature_managers.base import FeatureManager
-from msIO.features.gnps import FeatureGnpsNode
+from msIO.features.gnps import FeatureGnpsNode, GNPS_RENAME
 
 
 class GnpsImportManager(FeatureManager):
@@ -17,7 +16,17 @@ class GnpsImportManager(FeatureManager):
             path_file_gnps_graphml = self._find_gnps_file(path_gnps_folder)
         self.path_file_gnps_graphml = path_file_gnps_graphml
 
-        self._nodes: dict[int, dict] = self._get_nodes()
+        G: nx.Graph = nx.read_graphml(self.path_file_gnps_graphml)
+        keep_columns_to_dtype = {'name': int, 'componentindex': int, 'RTConsensus': float, 'precursor mass': float}
+        nodes_data = dict(map(lambda x: (int(x[0]), x[1]), G.nodes.data()))
+        self._df_nodes = (
+            pd.DataFrame(nodes_data)
+            .loc[list(keep_columns_to_dtype), :]
+            .T
+            .astype(keep_columns_to_dtype)
+            .rename(columns=GNPS_RENAME | dict(name='feature_id'))
+            .set_index('feature_id')
+        )
 
     @staticmethod
     def _find_gnps_file(path_gnps_folder):
@@ -26,22 +35,21 @@ class GnpsImportManager(FeatureManager):
                 return os.path.join(folder, file)
         raise FileNotFoundError(f'could not find graphml file in {folder}')
 
-    def _get_nodes(self) -> dict[int, dict]:
-        G: nx.Graph = nx.read_graphml(self.path_file_gnps_graphml)
-        return dict(map(lambda x: (int(x[0]), x[1]), G.nodes.data()))
-
     def _inner_missing_feature(self, f_id) -> None:
-        f = FeatureGnpsNode.from_graphml((f_id, self._nodes[f_id]))
+        f = FeatureGnpsNode(
+            feature_id=f_id,
+            **self._df_nodes.loc[f_id, :].to_dict(),
+        )
         self._features[f_id] = f
 
     @cached_property
     def feature_ids(self) -> np.ndarray[int]:
-        return np.array(sorted(self._get_nodes().keys()))
+        return self._df_nodes.index.values
 
 
 if __name__ == '__main__':
-    path_gnps_folder = r"\\hlabstorage.dmz.marum.de\scratch\Yannick\Guaymas\U1545B_U1549B\GNPS"
+    path_gnps_file = r"\\hlabstorage.dmz.marum.de\scratch\Yannick\Guaymas new method height recursive\GNPS\gnps.graphml"
 
-    gnps = GnpsImportManager(path_gnps_folder=path_gnps_folder)
+    gnps = GnpsImportManager(path_file_gnps_graphml=path_gnps_file)
 
     f = gnps.get_feature(1)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='msIO',
-    version='0.0.40',
+    version='0.0.41',
     url='',
     author='Yannick Zander',
     description='Reading and writing functions for different mass spectrometry '
