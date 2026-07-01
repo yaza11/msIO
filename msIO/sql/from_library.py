c6260b490904c6cff22b62555b7b91b5168418bb
yaza11 <116643078+yaza11@users.noreply.github.com>
2026-07-01 14:02:00 +0200

    fix function

--- msIO/sql/from_library.py
+++ msIO/sql/from_library.py
@@ -1,32 +1,30 @@
 import os
 
-from sqlalchemy import update
 from tqdm import tqdm
 
 from msIO import MSPReader
-from msIO.features.combined import FeatureCombined
-from msIO.features.metaboscape import FeatureMetaboScape
-from msIO.features.mgf import FeatureMgf
-from msIO.features.sirius import FeatureSirius
 from msIO.sql.session import initiate_db, get_sessionmaker
 
 
-def write_lib_from_msp_files(db_file: str, msp_files: list[str], commit_at_latest_after=10_000) -> None:
+def write_lib_from_msp_files(db_file: str, msp_files: list[str], commit_at_latest_after=10_000, low_memory_thr_GB = 1) -> None:
+    # create an sqlite file
     initiate_db(db_file)
 
+    # crate a connection to the database
     Session = get_sessionmaker(db_file)
-    f_id = 1
+    f_id: int = 1  # running index for features
     with Session() as session:
-        uncommited_features = 0
-        for library_file in library_files:
-            low_memory = os.path.getsize(library_file) > 1024 ** 3  # 1 GB
-            # low_memory = True
+        # keep track of uncommited features to avoid committing too often or running low on memory
+        uncommited_features: int = 0
+        for library_file in msp_files:
+            # determine whether
+            low_memory = os.path.getsize(library_file) > low_memory_thr_GB * 1024 ** 3  # 1 GB
             msp_manager = MSPReader(library_file, splitter_peaks_list=None, low_memory=low_memory)
             lib_name = library_file.split('\\')[-1].split('.')[0]
 
             for i in tqdm(
                     range(msp_manager.n_features),
-                    desc='adding features to DB',
+                    desc=f'adding features from {lib_name} to library',
                     total=msp_manager.n_features,
                     smoothing=1 / 50
             ):
@@ -42,13 +40,14 @@ def write_lib_from_msp_files(db_file: str, msp_files: list[str], commit_at_lates
                 session.add(f)
                 f_id += 1
                 uncommited_features += 1
-                if uncommited_features >= COMMIT_AT_LEAST_EVERY:
+                if uncommited_features >= commit_at_latest_after:
                     session.commit()
                     uncommited_features = 0
         session.commit()
 
 
 if __name__ == '__main__':
+    pass
     # from msIO.feature_managers.db import Library
 
     # lib = Library(db_file)
@@ -70,4 +69,5 @@ if __name__ == '__main__':
                         for f in os.listdir(folder_julius)
                         if (not f.startswith('!') and not ('test_as_H+' in f))
                     ]
+
 
