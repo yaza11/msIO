e92d824382f3741a63646839c1df0229195d5f6b
yaza11 <116643078+yaza11@users.noreply.github.com>
2026-09-15 17:00:31 +0200

    more test data some GUI support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='msIO',
-    version='0.0.83',
+    version='0.0.84',
     url='',
     author='Yannick Zander',
     description='Reading and writing functions for different mass spectrometry '
